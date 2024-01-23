db5a90a49158942158df83b6a39bd806dcb2caed
Sivaranjani Nandakumar <sivaranjanibharath87@gmail.com>
2024-01-23 11:46:29 -0600

    Added Assertion for task 1

--- client_test.py
+++ client_test.py
@@ -13,17 +13,20 @@ class ClientTest(unittest.TestCase):
       quotes[0]['stock'],
       float(quotes[0]['top_bid']['price']),
       float(quotes[0]['top_ask']['price']),
-      (121.2 + 120.48) / 2
-    )
-
+      (121.2 + 120.48) / 2)
     print("Actual result:", actual_result)
     print("Expected result:", expected_result)
+    actual_result1 = getDataPoint(quotes[1])
+    expected_result1 = (
+      quotes[1]['stock'],
+      float(quotes[1]['top_bid']['price']),
+      float(quotes[1]['top_ask']['price']),
+      (117.87 + 121.68) / 2)
+    print("Actual result1:", actual_result1)
+    print("Expected result1:", expected_result1)
+    self.assertEqual(actual_result, expected_result, expected_result1)
 
-    self.assertEqual(actual_result, expected_result)
 
-  #self.assertEqual(getDataPoint(quotes[0]), (quotes[0]['stock'], float(quotes[0]['top_bid']['price']), float(quotes[0]['top_ask']['price']), (121.2 + 120.48) /2))
-
-  # self.assertEqual(getDataPoint(quotes[1]), (quotes[1]['stock'], quotes[1]['top_bid']['price'], quotes[1]['top_ask']['price'], (quotes[1]['top_bid']['price'] + quotes[1]['top_ask']['price']) / 2))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -35,18 +38,21 @@ class ClientTest(unittest.TestCase):
       quotes[0]['stock'],
       float(quotes[0]['top_bid']['price']),
       float(quotes[0]['top_ask']['price']),
-      (120.48 + 119.2) / 2
-    )
-
+      (120.48 + 119.2) / 2)
     print("Actual result:", actual_result)
     print("Expected result:", expected_result)
+    actual_result1 = getDataPoint(quotes[1])
+    expected_result1 = (
+      quotes[1]['stock'],
+      float(quotes[1]['top_bid']['price']),
+      float(quotes[1]['top_ask']['price']),
+      (117.87 + 121.68) / 2)
+    print("Actual result1:", actual_result1)
+    print("Expected result1:", expected_result1)
+    self.assertEqual(actual_result, expected_result, expected_result1)
 
-    self.assertEqual(actual_result, expected_result)
 
-    # self.assertEqual(getDataPoint(quotes[0]), (quotes[0]['stock'], quotes[0]['top_bid']['price'], quotes[0]['top_ask']['price'],(quotes[0]['top_bid']['price'] + quotes[0]['top_ask']['price']) / 2))
-    # self.assertEqual(getDataPoint(quotes[1]), (quotes[1]['stock'], quotes[1]['top_bid']['price'], quotes[1]['top_ask']['price'],(quotes[1]['top_bid']['price'] + quotes[1]['top_ask']['price']) / 2))
-  """ ------------ Add more unit tests ------------ """
-
+""" ------------ Add more unit tests ------------ """
 
 
 if __name__ == '__main__':
